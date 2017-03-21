70dcab2b8ac1869ec4573a4d146ca3cb477fe0d8
Leonard <leonardj@msoe.edu>
2017-03-21 07:09:29 -0500

    init commit

---
+++ www/js/app.js
@@ -0,0 +1,76 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'NavCtrl'
+  })
+
+  .state('app.trails', {
+    url: '/trails',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/trails.html',
+        controller: 'TrailsCtrl'
+      }
+    }
+  })
+
+  .state('app.ridges', {
+      url: '/ridges',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/ridges.html',
+          controller: 'RidgesCtrl'
+        }
+      }
+    })
+
+    .state('app.discovery', {
+        url: '/discovery',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/discovery.html',
+            controller: 'DisCtrl'
+          }
+        }
+      })
+
+    .state('app.map', {
+      url: '/map',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/map.html',
+          controller: 'MapCtrl'
+        }
+      }
+    });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/trails');
+});
